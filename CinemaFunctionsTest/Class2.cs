3500a8ef9668201a8ead5f46d7a9616df3d17e0d
Kenny <kenny-williamsStockd@live.nmit.ac.nz>
2021-08-28 15:17:37 +1200

    added and tested first test case

--- CinemaFunctionsTest/Class2.cs
+++ CinemaFunctionsTest/Class2.cs
@@ -1,15 +1,30 @@
-﻿using NUnit.Framework;
+﻿using CinemaApp;
+using NUnit.Framework;
 
 namespace CinemaFunctionsTest
 {
     [TestFixture]
-    public class Class2
+    public class TestCase1
     {
-        [Test]
-        public void Test1()
+        //Adult_Before_5(int quantitiy, string person, string day, decimal time, decimal expected) 
+        [TestCase(1, "Adult", "Monday", 4, 14.50)]
+        [TestCase(2, "Adult", "Monday", 4, 29.00)]
+        [TestCase(0, "Adult", "Monday", 4, -1)]
+        [TestCase(4, "Adult", "Monday", 5, -1)]
+        [TestCase(4, "Adult", "Monday", 4, 58.00)]
+
+
+        public void Adult_Before_5(int pr_quantity, string pr_person, string pr_day, decimal pr_time, decimal expected)
         {
-            Assert.That(1 == 1);
+            //arange
+            TicketPriceController TPC = new();
+            //act
+            decimal price = TPC.Adult_Before_5(pr_quantity, pr_person, pr_day, pr_time);
+            //assert
+            Assert.That(price == expected);
+
         }
+        
 
     }
 }
