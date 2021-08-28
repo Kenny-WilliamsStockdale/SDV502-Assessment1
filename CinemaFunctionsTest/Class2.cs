6cafb11d17c90584106cb3201463d8f4c593ed62
Kenny <kenny-williamsStockd@live.nmit.ac.nz>
2021-08-28 13:38:34 +1200

    added dependancies and function code

--- CinemaFunctionsTest/Class2.cs
+++ CinemaFunctionsTest/Class2.cs
@@ -1,8 +1,15 @@
-﻿using System;
+﻿using NUnit.Framework;
 
 namespace CinemaFunctionsTest
 {
+    [TestFixture]
     public class Class2
     {
+        [Test]
+        public void Test1()
+        {
+            Assert.That(1 == 1);
+        }
+
     }
 }
