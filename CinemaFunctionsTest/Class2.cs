f8b9666473731bf646f82c189961ff5c6e45b119
Kenny <kenny-williamsStockd@live.nmit.ac.nz>
2021-08-29 14:58:05 +1200

    added last few test cases

--- CinemaFunctionsTest/Class2.cs
+++ CinemaFunctionsTest/Class2.cs
@@ -31,7 +31,7 @@ namespace CinemaFunctionsTest
     public class TestCase2
     {
         //Adult_After_5(int quantitiy, string person, string day, decimal time, decimal expected) 
-        [TestCase(1, "child", "Monday", 4, -1)]
+        [TestCase(1, "adult", "Tuesday", 4, -1)]
         [TestCase(2, "adult", "Sunday", 4, -1)]
         [TestCase(1, "adult", "Monday", 6, 17.50)]
         [TestCase(4, "adult", "Monday", 5, 70.00)]
@@ -78,7 +78,7 @@ namespace CinemaFunctionsTest
 
     public class TestCase4
     {
-        //Adult_Tuesday(int quantitiy, string person, decimal expected) 
+        //Child_Under_16(int quantitiy, string person, decimal expected) 
         [TestCase(1, "child", 12.00)]
         [TestCase(4, "child", 48.00)]
         [TestCase(1, "adult", -1)]
@@ -92,6 +92,126 @@ namespace CinemaFunctionsTest
             TicketPriceController TPC = new();
             //act
             decimal price = TPC.Child_Under_16(pr_quantity, pr_person);
+            //assert
+            Assert.That(price == expected);
+
+        }
+
+
+    }
+
+    public class TestCase5
+    {
+        //Senior(int quantitiy, string person, decimal expected) 
+        [TestCase(2, "child", -1)]
+        [TestCase(2, "senior", 25.00)]
+        [TestCase(1, "adult", -1)]
+        [TestCase(4, "senior", 50.00)]
+        [TestCase(2, "student", -1)]
+
+
+        public void Senior(int pr_quantity, string pr_person, decimal expected)
+        {
+            //arange
+            TicketPriceController TPC = new();
+            //act
+            decimal price = TPC.Senior(pr_quantity, pr_person);
+            //assert
+            Assert.That(price == expected);
+
+        }
+
+
+    }
+
+    public class TestCase6
+    {
+        //Student(int quantitiy, string person, decimal expected) 
+        [TestCase(1, "student", 14.00)]
+        [TestCase(2, "senior", -1)]
+        [TestCase(3, "adult", -1)]
+        [TestCase(4, "student", 56.00)]
+        [TestCase(2, "student", 28.00)]
+
+
+        public void Student(int pr_quantity, string pr_person, decimal expected)
+        {
+            //arange
+            TicketPriceController TPC = new();
+            //act
+            decimal price = TPC.Student(pr_quantity, pr_person);
+            //assert
+            Assert.That(price == expected);
+
+        }
+
+
+    }
+
+    public class TestCase7
+    {
+        //Family_Pass(int pr_quantity_ticket, int pr_quantity_adult, int pr_quantity_child, decimal expected) 
+        [TestCase(1, 2, 2, 46.00)]
+        [TestCase(1, 1, 3, 46.00)]
+        [TestCase(2, 2, 6, -1)]
+        [TestCase(1, 3, 1, -1)]
+        [TestCase(2, 4, 4, -1)]
+
+
+        public void Family_Pass(int pr_quantity_ticket, int pr_quantity_adult, int pr_quantity_child, decimal expected)
+        {
+            //arange
+            TicketPriceController TPC = new();
+            //act
+            decimal price = TPC.Family_Pass(pr_quantity_ticket, pr_quantity_adult, pr_quantity_child);
+            //assert
+            Assert.That(price == expected);
+
+        }
+
+
+    }
+
+    public class TestCase8
+    {
+        //Chick_Flick_Thursday(int pr_quantity, string pr_person, string pr_day, decimal expected) 
+        [TestCase(1, "adult", "thursday", 21.50)]
+        [TestCase(1, "student", "thursday", -1)]
+        [TestCase(2, "adult", "monday", -1)]
+        [TestCase(2, "adult", "thursday", 43.00)]
+        [TestCase(4, "adult", "thursday", 86.00)]
+
+
+        public void Chick_Flick_Thursday(int pr_quantity, string pr_person, string pr_day, decimal expected)
+        {
+            //arange
+            TicketPriceController TPC = new();
+            //act
+            decimal price = TPC.Chick_Flick_Thursday( pr_quantity, pr_person, pr_day);
+            //assert
+            Assert.That(price == expected);
+
+        }
+
+
+    }
+
+    public class TestCase9
+    {
+        //Kids_Careers(int pr_quantity, string pr_day, bool pr_holiday, decimal expected) 
+        [TestCase(1, "thursday", false, -1)]
+        [TestCase(1, "wednesday", false, 12.00)]
+        [TestCase(2, "wednesday", true, -1)]
+        [TestCase(2, "wednesday", false, 24.00)]
+        [TestCase(5, "wednesday", false, 60.00)]
+
+
+        public void Kids_Careers(int pr_quantity, string pr_day, bool pr_holiday, decimal expected)
+        {
+            //arange
+            TicketPriceController TPC = new();
+            //act
+            decimal price = TPC.Kids_Careers(pr_quantity, pr_day, pr_holiday);
             //assert
             Assert.That(price == expected);
 
